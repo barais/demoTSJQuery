1c2ba9fa88ee52b04d7e7104224ba112d851e1ec
Olivier Barais <olivier.barais@gmail.com>
2019-04-24 16:04:00 +0200

    new

---
+++ public/app.ts
@@ -0,0 +1,29 @@
+class Demo {
+    message: string;
+
+    constructor(mes: string) {
+        this.message = mes;
+    }
+
+    ClickGo() {
+        $("#lblGo").text("You pressed the Go button");
+    }
+
+    ClickInfo() {
+        $("#Info").text(this.message);
+        $("#Info").fadeOut("slow");
+        $("#Info").fadeIn(2000);
+    }
+}
+
+let dem = new Demo("bla bla bla bla");
+
+$(document).ready(function () {
+    $('#btnGo').click(function () {
+        dem.ClickGo();
+    });
+    //$("#btnShowInfo").click(dem.ClickInfo); // doesn't work
+    $("#btnShowInfo").click(function () {
+        dem.ClickInfo();
+    });
+});
